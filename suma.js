d1f9deba21b1f55adc7ac3b9c5502102b7a5eb74
ccmarcos <marcosantonio199189@gmail.com>
2017-03-25 14:45:29 -0600

    graphic level 5 dynamic

--- suma.js
+++ suma.js
@@ -1,14 +1,39 @@
 ejercicio1();
 
 var c=0;
+
 function newInput(){
-  var inpt = document.createElement('input');
-  inpt.type = "text";
-  inpt.name = "input_"+c;
-  inpt.id = "input_"+c;
+  var restriction = document.getElementById("restric").value;
+  var inpt1 = document.createElement('input');
+  var inpt2 = document.createElement('input');
+  var inpt3 = document.createElement('input');
+
+  if(restriction){
+  for(var i=0; i<restriction; i++){
+  inpt1.type = "text";
+  inpt1.name = "input1_"+c;
+  inpt1.id = "input1_"+c;
+
+  document.f1.appendChild(inpt1);
+  document.f1.innerHTML+="X+";
+
+  inpt2.type="text";
+  inpt2.name = "input2_"+c;
+  inpt2.id = "input2_"+c;
+
+  document.f1.appendChild(inpt2);
+  document.f1.innerHTML+="Y=";
+
+  inpt3.type = "text";
+  inpt3.name = "input3_"+c;
+  inpt3.id = "input3_"+c;
   c+=1;
-  document.f1.appendChild(inpt);
-  document.f1.innerHTML+="<br/>";
+
+  document.f1.appendChild(inpt3);
+  document.f1.innerHTML+="<br/>"
+}
+
+}
 }
 
 
